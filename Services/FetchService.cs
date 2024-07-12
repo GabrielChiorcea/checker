d446c325719ddfc67961d7e5438e7e247e7b5ded
Gabriel Chiorcea <gabrielchiorcea93@gmail.com>
2024-07-12 12:09:48 +0300

    JWT prepare

--- Services/FetchService.cs
+++ Services/FetchService.cs
@@ -1,6 +1,10 @@
 
+using System.Net;
 using System.Net.Http.Json;
+using System.Text;
 using Checker.Models;
+using Microsoft.JSInterop;
+
 
 
 namespace Checker.Services
@@ -8,9 +12,11 @@ namespace Checker.Services
     public class FetchService
     {
         private readonly HttpClient _httpClinet;
+        private readonly IJSRuntime _jsRuntime;
 
-        public FetchService(HttpClient httpClient){
+        public FetchService(HttpClient httpClient, IJSRuntime jSRuntime){
             _httpClinet = httpClient;
+            _jsRuntime = jSRuntime;
         }
 
         public async Task<JsonModel> LoginAsync(LoginModel loginModel){
@@ -26,6 +32,7 @@ namespace Checker.Services
         public async Task<JsonModel> SingUpAsyc(SingUpModel singUpModel){
 
             var response = await _httpClinet.PostAsJsonAsync("http://127.0.0.1:3000/intra-in-cont", singUpModel);
+                        
             response.EnsureSuccessStatusCode();
 
             var token = await response.Content.ReadFromJsonAsync<JsonModel>();
@@ -40,5 +47,28 @@ namespace Checker.Services
 
             return state;
         }
+
+    private async Task<string> GetCsrfToken()
+    {
+        return await _jsRuntime.InvokeAsync<string>("getCookie", "csrfToken");
+    }
+
+        public async Task<JsonModel> GetRespons(string url)
+        {   
+            var csf = await GetCsrfToken();
+            using HttpClient client = new HttpClient();
+            string jsonContent = $"{{\"val\":\"{csf}\"}}";
+            using HttpRequestMessage req = new HttpRequestMessage( HttpMethod.Post ,url){
+                Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+            };
+              HttpResponseMessage response = await client.SendAsync(req);
+                response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadFromJsonAsync<JsonModel>();
+
+            return responseBody; 
+        }
+
+
     }
 }
