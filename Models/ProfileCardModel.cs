598e74254f90d32a8e4c55ec800fd5ef144f4194
Gabriel Chiorcea <gabrielchiorcea93@gmail.com>
2024-08-04 19:22:20 +0300

    User settings and profile

---
+++ Models/ProfileCardModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Checker.Models
+{
+    public class ProfileCardModel
+    {
+        public byte[] Image { get; set; }
+        public string Occupation { get; set; }
+        public string HomeAddress { get; set; }
+        public string Country { get; set; }
+        public string County { get; set; }
+        public string  Field { get; set; }
+    }
+}
---
+++ Models/SocialMediaModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checker.Models
+{
+    public class SocialMediaModel
+    {
+        public string LinkedIn { get; set; }
+        public string FaceBook { get; set; }
+        public string GitHub { get; set; }
+        public string Instagram { get; set; }
+        public string Twitter { get; set; }
+        public string Youtube { get; set; }
+        public string Description { get; set; }
+
+    }
+}
